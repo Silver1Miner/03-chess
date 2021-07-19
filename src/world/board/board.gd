d9606a95388435e7f484eea3b39cd2bbece1f42a
User <silver1miner@gmail.com>
2021-07-18 20:01:16 -0700

    legal moves and checkmate detection

--- src/world/board/board.gd
+++ src/world/board/board.gd
@@ -8,14 +8,19 @@ onready var player = $player
 
 var board_state := []
 var board_display := []
-var prev_board_state := []
-var prev_board_display := []
 
 var selected_piece = null
 var moves := []
 var currently_attacked_by_blue := []
 var currently_attacked_by_red := []
 var currently_attacked_by_green := []
+
+var endgame_state := "playing"
+var move_log := []
+var current_turn := "blue"
+var blue_in_check := false
+var red_in_check := false
+var green_in_check := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,6 +31,9 @@ func _ready() -> void:
 	initialize_board_state()
 	populate_state_from_board()
 	calculate_current_attacked_squares()
+	check()
+	endgame_state = checkmate_stalemate_checker()
+	print(endgame_state)
 
 func initialize_board_state() -> void:
 	for x in range(9):
@@ -49,14 +57,14 @@ func _on_accept_pressed(cell, team) -> void:
 			print("move ", selected_piece.piece_name, " to ", cell)
 			move_display.clear()
 			move_from_to(selected_piece.cell, cell)
-		elif board_display[cell.x][cell.y] == "":
+		elif board_state[cell.x][cell.y] == null:
 			selected_piece = null
 			move_display.clear()
 		elif team == board_state[cell.x][cell.y].team:
 			selected_piece = board_state[cell.x][cell.y]
 			calculate_piece_movement(cell)
 		return
-	if board_display[cell.x][cell.y] != "":
+	if board_state[cell.x][cell.y] != null:
 		print(board_state[cell.x][cell.y])
 		if team == board_state[cell.x][cell.y].team:
 			selected_piece = board_state[cell.x][cell.y]
@@ -87,8 +95,9 @@ func _on_cancel_pressed(cell, team) -> void:
 			attacks = units.calculate_rook_attacks(piece.cell)
 	move_display.draw_attacked(attacks)
 
-func _on_player_moved(cell, team) -> void:
-	print(team, " moved cursor to ", cell)
+func _on_player_moved(_cell, _team) -> void:
+	#print(team, " moved cursor to ", cell)
+	pass
 
 func move_from_to(start_cell, end_cell) -> void:
 	board_state[start_cell.x][start_cell.y].move_along_path([start_cell, end_cell])
@@ -106,6 +115,7 @@ func move_from_to(start_cell, end_cell) -> void:
 			board_state[end_cell.x][end_cell.y].change_to_queen()
 		elif board_state[end_cell.x][end_cell.y].team == "green" and end_cell.y < 3:
 			board_state[end_cell.x][end_cell.y].change_to_queen()
+	selected_piece = null
 	calculate_current_attacked_squares()
 	check()
 	next_turn()
@@ -115,7 +125,7 @@ func calculate_piece_movement(cell: Vector2) -> void:
 	print(board_state[cell.x][cell.y].piece_name)
 	match board_state[cell.x][cell.y].piece_type:
 		"king":
-			moves = units.calculate_king_move(cell, board_state[cell.x][cell.y].team)
+			moves = units.calculate_king_move(cell)
 		"bishop":
 			moves = units.calculate_bishop_move(cell)
 		"knight":
@@ -171,39 +181,139 @@ func _unhandled_input(event) -> void:
 func next_turn() -> void:
 	if player.team == "blue":
 		player.set_team("red")
+		current_turn = "red"
 		print("Red's turn")
 	elif player.team == "red":
 		player.set_team("green")
+		current_turn = "green"
 		print("Green's turn")
 	else:
 		player.set_team("blue")
+		current_turn = "blue"
 		print("Blue's turn")
+	endgame_state = checkmate_stalemate_checker()
 
 func check() -> void:
 	var blue_checks = []
 	for sq in currently_attacked_by_blue:
-		if board_display[sq.x][sq.y] == "king_red":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_red":
 			print("Red in check")
+			red_in_check = true
 			blue_checks.append(sq)
-		if board_display[sq.x][sq.y] == "king_green":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_green":
 			print("Green in check")
+			green_in_check = true
 			blue_checks.append(sq)
 	move_display.add_draw_attacked(blue_checks)
 	var red_checks = []
 	for sq in currently_attacked_by_red:
-		if board_display[sq.x][sq.y] == "king_blue":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_blue":
 			print("Blue in check")
+			blue_in_check = true
 			red_checks.append(sq)
-		if board_display[sq.x][sq.y] == "king_green":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_green":
 			print("Green in check")
+			green_in_check = true
 			red_checks.append(sq)
 	move_display.add_draw_attacked(red_checks)
 	var green_checks = []
 	for sq in currently_attacked_by_green:
-		if board_display[sq.x][sq.y] == "king_red":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_red":
 			print("Red in check")
+			red_in_check = true
 			green_checks.append(sq)
-		if board_display[sq.x][sq.y] == "king_blue":
+		if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_blue":
 			print("Blue in check")
+			blue_in_check = true
 			green_checks.append(sq)
 	move_display.add_draw_attacked(green_checks)
+
+func would_not_end_in_check(cell, new_cell) -> bool:
+	var team = board_state[cell.x][cell.y].team
+	var in_check = true
+	var prev_state = board_state.duplicate(true)
+	board_state[cell.x][cell.y].cell = new_cell
+	if board_state[new_cell.x][new_cell.y] != null:
+		board_state[new_cell.x][new_cell.y].cell = null
+	board_state[new_cell.x][new_cell.y] = board_state[cell.x][cell.y]
+	board_state[cell.x][cell.y] = null
+	calculate_current_attacked_squares()
+	match team:
+		"blue":
+			for sq in currently_attacked_by_red:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_blue":
+					in_check = false
+			for sq in currently_attacked_by_green:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_blue":
+					in_check = false
+		"red":
+			for sq in currently_attacked_by_blue:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_red":
+					in_check = false
+			for sq in currently_attacked_by_green:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_red":
+					in_check = false
+		"green":
+			for sq in currently_attacked_by_blue:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_green":
+					in_check = false
+			for sq in currently_attacked_by_red:
+				if board_state[sq.x][sq.y] != null and board_state[sq.x][sq.y].piece_name == "king_green":
+					in_check = false
+	board_state = prev_state.duplicate(true)
+	board_state[cell.x][cell.y].cell = cell
+	if board_state[new_cell.x][new_cell.y] != null:
+		board_state[new_cell.x][new_cell.y].cell = new_cell
+	return in_check
+
+func checkmate_stalemate_checker() -> String:
+	var all_blue_moves = get_all_legal_moves("blue")
+	var all_red_moves = get_all_legal_moves("red")
+	var all_green_moves = get_all_legal_moves("green")
+	match current_turn:
+		"blue":
+			if not all_blue_moves:
+				if blue_in_check:
+					print("blue lost")
+					return "blue checkmated"
+				else:
+					print("stalemate")
+					return "stalemate"
+		"red":
+			if not all_red_moves:
+				if red_in_check:
+					print("red lost")
+					return "red checkmated"
+				else:
+					print("stalemate")
+					return "stalemate"
+		"green":
+			if not all_green_moves:
+				if green_in_check:
+					print("green lost")
+					return "green checkmated"
+				else:
+					print("stalemate")
+					return "stalemate"
+	return "playing"
+
+func get_all_legal_moves(team) -> Array:
+	var legal_moves = []
+	for piece in units.get_children():
+		if piece.team == team:
+			var piece_moves = []
+			match piece.piece_type:
+				"king":
+					piece_moves = units.calculate_king_move(piece.cell)
+				"bishop":
+					piece_moves = units.calculate_bishop_move(piece.cell)
+				"knight":
+					piece_moves = units.calculate_knight_move(piece.cell)
+				"pawn":
+					piece_moves = units.calculate_pawn_move(piece.cell)
+				"queen":
+					piece_moves = units.calculate_queen_move(piece.cell)
+				"rook":
+					piece_moves = units.calculate_rook_move(piece.cell)
+			legal_moves += piece_moves
+	return legal_moves
--- src/world/board/units.gd
+++ src/world/board/units.gd
@@ -3,24 +3,24 @@ extends Node2D
 onready var board = get_parent()
 export var grid: Resource = preload("res://src/world/board/Grid.tres")
 
-func calculate_king_move(cell: Vector2, team) -> Array:
+func calculate_king_move(cell: Vector2) -> Array:
 	var moves = []
 	var possible = [Vector2(-1,-1), Vector2(-1,0),
 	Vector2(-1,1), Vector2(0,-1), Vector2(0,1),
 	Vector2(1,-1), Vector2(1,0), Vector2(1,1)]
 	for p in possible:
 		var new_cell = cell + p
-		if grid.is_within_bounds(new_cell):
-			if team == "blue":
+		if grid.is_within_bounds(new_cell) and board.would_not_end_in_check(cell, new_cell):
+			if board.board_state[cell.x][cell.y].team == "blue":
 				if new_cell in board.currently_attacked_by_red or new_cell in board.currently_attacked_by_green:
 					continue
-			if team == "red":
+			if board.board_state[cell.x][cell.y].team == "red":
 				if new_cell in board.currently_attacked_by_blue or new_cell in board.currently_attacked_by_green:
 					continue
-			if team == "green":
+			if board.board_state[cell.x][cell.y].team == "green":
 				if new_cell in board.currently_attacked_by_red or new_cell in board.currently_attacked_by_blue:
 					continue
-			if board.board_display[new_cell.x][new_cell.y] == "":
+			if board.board_state[new_cell.x][new_cell.y] == null:
 				moves.append(new_cell)
 			elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 				moves.append(new_cell)
@@ -34,9 +34,9 @@ func calculate_bishop_move(cell: Vector2) -> Array:
 		for i in range(1,9):
 			var new_cell = cell + p * i
 			if grid.is_within_bounds(new_cell):
-				if board.board_display[new_cell.x][new_cell.y] == "":
+				if board.board_state[new_cell.x][new_cell.y] == null and board.would_not_end_in_check(cell, new_cell):
 					moves.append(new_cell)
-				elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
+				elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team and board.would_not_end_in_check(cell, new_cell):
 					moves.append(new_cell)
 					break
 				else:
@@ -50,8 +50,8 @@ func calculate_knight_move(cell: Vector2) -> Array:
 	Vector2(2,-1), Vector2(-1,-2), Vector2(1,-2)]
 	for p in possible:
 		var new_cell = cell + p
-		if grid.is_within_bounds(new_cell):
-			if board.board_display[new_cell.x][new_cell.y] == "":
+		if grid.is_within_bounds(new_cell) and board.would_not_end_in_check(cell, new_cell):
+			if board.board_state[new_cell.x][new_cell.y] == null:
 				moves.append(new_cell)
 			elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 				moves.append(new_cell)
@@ -65,12 +65,13 @@ func calculate_pawn_move(cell: Vector2) -> Array:
 		possible_cap = [Vector2(-1, -1), Vector2(1, -1)]
 		possible_mov = Vector2(0, -1)
 	var new_cell = cell + possible_mov
-	if grid.is_within_bounds(new_cell) and board.board_display[new_cell.x][new_cell.y] == "":
-		moves.append(new_cell)
+	if grid.is_within_bounds(new_cell) and board.would_not_end_in_check(cell, new_cell):
+		if board.board_state[new_cell.x][new_cell.y] == null:
+			moves.append(new_cell)
 	for p in possible_cap:
 		new_cell = cell + p
-		if grid.is_within_bounds(new_cell) and board.board_display[new_cell.x][new_cell.y] != "":
-			if board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
+		if grid.is_within_bounds(new_cell) and board.would_not_end_in_check(cell, new_cell):
+			if board.board_state[new_cell.x][new_cell.y] != null and board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 				moves.append(new_cell)
 	return moves
 
@@ -81,8 +82,8 @@ func calculate_queen_move(cell: Vector2) -> Array:
 	Vector2(1,-1), Vector2(1,0), Vector2(1,1)]
 	for p in possible:
 		var new_cell = cell + p
-		if grid.is_within_bounds(new_cell):
-			if board.board_display[new_cell.x][new_cell.y] == "":
+		if grid.is_within_bounds(new_cell) and board.would_not_end_in_check(cell, new_cell):
+			if board.board_state[new_cell.x][new_cell.y] == null:
 				moves.append(new_cell)
 			elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 				moves.append(new_cell)
@@ -96,9 +97,9 @@ func calculate_rook_move(cell: Vector2) -> Array:
 		for i in range(1,9):
 			var new_cell = cell + p * i
 			if grid.is_within_bounds(new_cell):
-				if board.board_display[new_cell.x][new_cell.y] == "":
+				if board.board_state[new_cell.x][new_cell.y] == null and board.would_not_end_in_check(cell, new_cell):
 					moves.append(new_cell)
-				elif board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
+				elif board.board_state[new_cell.x][new_cell.y] != null and board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team and board.would_not_end_in_check(cell, new_cell):
 					moves.append(new_cell)
 					break
 				else:
@@ -124,7 +125,9 @@ func calculate_bishop_attacks(cell: Vector2) -> Array:
 		for i in range(1,9):
 			var new_cell = cell + p * i
 			if grid.is_within_bounds(new_cell):
-				if board.board_display[new_cell.x][new_cell.y] == "":
+				if board.board_state[new_cell.x][new_cell.y] == null:
+					attacks.append(new_cell)
+				elif board.board_state[new_cell.x][new_cell.y].piece_type == "king" and board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 					attacks.append(new_cell)
 				else:
 					attacks.append(new_cell)
@@ -145,7 +148,7 @@ func calculate_knight_attacks(cell: Vector2) -> Array:
 func calculate_pawn_attacks(cell: Vector2) -> Array:
 	var attacks = []
 	var possible_cap = [Vector2(-1, 1), Vector2(1, 1)]
-	if board.board_state[cell.x][cell.y].team != "blue":
+	if board.board_state[cell.x][cell.y] != null and board.board_state[cell.x][cell.y].team != "blue":
 		possible_cap = [Vector2(-1, -1), Vector2(1, -1)]
 	for p in possible_cap:
 		var new_cell = cell + p
@@ -172,7 +175,9 @@ func calculate_rook_attacks(cell: Vector2) -> Array:
 		for i in range(1,9):
 			var new_cell = cell + p * i
 			if grid.is_within_bounds(new_cell):
-				if board.board_display[new_cell.x][new_cell.y] == "":
+				if board.board_state[new_cell.x][new_cell.y] == null:
+					attacks.append(new_cell)
+				elif board.board_state[new_cell.x][new_cell.y].piece_type == "king" and board.board_state[new_cell.x][new_cell.y].team != board.board_state[cell.x][cell.y].team:
 					attacks.append(new_cell)
 				else:
 					attacks.append(new_cell)
